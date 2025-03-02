5d2e2ca16090baf814b11acfd27749ed81a37d57
Kauan Farias <kauandefarias@yahoo.com.br>
2025-03-02 20:54:35 -0300

    ✨ feat: adicionando favicon e logo e começando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
+import logo from '../assets/logo-500.png'
 
 function App() {
   return (
-    <h1 className="font-bold text-2xl mt-5 text-center">Olá, mundo!</h1>
+    <>
+      <header className="p-2 bg-linear-to-bl from-stone-900 to-stone-950">
+        <img src={logo} className="w-12 m-1" alt="Logotipo do site" title="Logotipo" />
+      </header>
+    </>
   )
 }
 
