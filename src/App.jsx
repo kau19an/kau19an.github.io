fef6cb7aff0671329d29f146b2e207d79882eeba
Kauan Farias <kauandefarias@yahoo.com.br>
2025-03-03 12:40:41 -0300

    ✨ feat: adicionando ícones do FA externamente e GH no header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import './App.css'
-import logo from '../assets/logo-500.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './App.css';
+import logo from '../assets/logo-500.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icons } from './utils/icons.jsx';
 
 function App() {
   return (
-    <>
-      <header className="p-2 bg-linear-to-bl from-stone-900 to-stone-950">
-        <img src={logo} className="w-12 m-1" alt="Logotipo do site" title="Logotipo" />
+    <div className="dark:bg-stone-950 transition-colors duration-300">
+      <header className="flex gap-2 items-center p-2 bg-linear-to-bl from-stone-900 to-stone-950">
+        <a href="/" target="_self">
+          <img src={logo} className="w-12 m-1 hover:opacity-75" alt="Logotipo do site" title="Logotipo" draggable="false"/>
+        </a>
+        <a href="https://github.com/kau19an/kau19an.github.io" target="_blank">
+          <FontAwesomeIcon icon={icons.gh} className="text-gray-300 hover:text-gray-400 text-lg" title="Repositório no GitHub" />
+        </a>
       </header>
-    </>
-  )
+      <main></main>
+      <footer></footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/icons.jsx
@@ -0,0 +1,26 @@
+import { // Social medias
+    faBluesky, faDiscord, faFacebook, faGithub, faInstagram, faXTwitter, faYoutube
+} from '@fortawesome/free-brands-svg-icons';
+
+import { // UI
+    faBars, faCaretDown, faHeart, faMoon, faSun, faCaretUp
+} from '@fortawesome/free-solid-svg-icons';
+
+export const icons = {
+    // Social medias
+    bs: faBluesky,
+    dc: faDiscord,
+    fb: faFacebook,
+    gh: faGithub,
+    ig: faInstagram,
+    tw: faXTwitter,
+    yt: faYoutube,
+
+    // UI
+    bars: faBars,
+    down: faCaretDown,
+    heart: faHeart,
+    moon: faMoon,
+    sun: faSun,
+    up: faCaretUp,
+};
